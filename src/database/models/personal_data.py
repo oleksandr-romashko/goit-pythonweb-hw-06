d6131ff08557c4d0bb4fccac41a946ac39110358
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-08-01 17:41:46 +0200

    Add personal data orm model

---
+++ src/database/models/personal_data.py
@@ -0,0 +1,30 @@
+"""
+ORM abstract model for general person-related fields.
+"""
+
+import datetime
+
+from sqlalchemy import String, Date
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base_model import BaseModel
+
+
+class PersonalData(BaseModel):
+    """
+    Table to store personal information that can be linked to multiple entity types
+    (e.g. Student, Teacher). Useful for GDPR compliance (anonymization, soft delete, etc).
+    """
+
+    __tablename__ = "personal_data"
+
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    birth_date: Mapped[datetime.date] = mapped_column(Date, nullable=False)
+    phone_number: Mapped[str | None] = mapped_column(String(20), nullable=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    photo_url: Mapped[str | None] = mapped_column(String(255), nullable=True)
+    bio: Mapped[str | None] = mapped_column(String(1000), nullable=True)
+
+    # TODO: Add address -> foreign key to a separate table (database normalization)
+    # TODO: Add gender (if necessary) -> enum male/female/other to allow filtering/statistics
