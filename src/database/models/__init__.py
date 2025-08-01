ded0ee7ef212485fe71ec20895ebeb129a36c07e
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-08-02 00:06:43 +0200

    Update model reexport

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -1,4 +1,17 @@
 from .student import Student
 from .teacher import Teacher
+from .personal_data import PersonalData
+from .group import Group
+from .subject import Subject
+from .grade import Grade
+from .associations import group_subject_association_table
 
-__all__ = ["Student", "Teacher"]
+__all__ = [
+    "Student",
+    "Teacher",
+    "PersonalData",
+    "Group",
+    "Subject",
+    "Grade",
+    "group_subject_association_table",
+]
